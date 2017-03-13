ebc6c72c1907ccbbd2f4e16fcb898fb3574fdd6d
felwijaya <fwijaya06@student.ciputra.ac.id>
2017-03-13 13:13:00 +0700

    test

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<title> ToDO List </title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
 	<div class="container"> 
@@ -10,15 +11,24 @@
 			<div class="col-lg-12">
 				<h1> ToDo List </h1>
 				<ul>
-					<li> Taki </li>
-					<li> Mitsuha </li>
-					<li> Yotsuha </li>
+					@foreach($todo as $td)
+					<li>{{ $td->item }}</li>
+					@endforeach
 				</ul>
-				<form>
-					<div class="form-group">
-						<input type="text" class="form-control" name="todo_item" placeholder="Item baru. . .">
+				<form action="todo" method="POST">
+					{{ csrf_field() }}
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="form-group">
+								<input type="text" class="form-control" name="todo_item" placeholder="Item baru. . .">
+								</div>
+							</div>
+							<div class="col-lg-1">
+								<button type="submit" class="btn btn-success"> Add </button>
+							</div>
+							<div class="col-lg-8">
+						</div>
 					</div>
-					<button type="submit" class="btn btn-success"> Add </button>
 				</form>
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 			</div>
